5a809f15a545de9a407dfe406e6cd5cb3529b774
Melkorich <Kirill.Lavrov92@gmail.com>
2022-02-06 19:29:07 +0300

    adaptiv finished

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 'use strict'
 
+//accordion
 
 const accordionBtn = document.querySelectorAll('.accordion__btn');
 
@@ -24,6 +25,7 @@ accordionBtn.forEach(accordionBtn => {
   });
 });
 
+//swiper slider
 
 const swiper = new Swiper('.swiper', {
   loop: false,
@@ -33,3 +35,56 @@ const swiper = new Swiper('.swiper', {
     dynamicBullets: true,
   },
 });
+
+
+//burger menu
+
+const burger = document.querySelector('.menu__burger');
+const menuList = document.querySelector('.menu__list');
+const menuLink = document. querySelectorAll('.menu__link');
+
+
+burger.addEventListener('click', function() {
+  this.classList.toggle('active');
+  menuList.classList.toggle('menu__list--active');
+  document.querySelector('body').classList.toggle('no-scroll');
+})
+
+
+for (let i=0; i<menuLink.length; i++) {
+  menuLink[i].addEventListener('click', function() {
+    menuList.classList.remove('menu__list--active');
+    document.querySelector('body').classList.remove('no-scroll');
+    burger.classList.remove('active');
+  });
+}
+
+
+
+//modal window
+
+const modal = document.querySelector('.modal');
+const modalOpen = document.querySelector('.modal-open');
+const modalClose = document.querySelector('.modal-close');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+const openModal = function() {
+  modal.classList.remove('hidden');
+  modalOverlay.classList.remove('hidden');
+}
+
+const closeModal = function() {
+  modal.classList.add('hidden');
+  modalOverlay.classList.add('hidden');
+}
+
+modalOpen.addEventListener('click', openModal);
+modalClose.addEventListener('click', closeModal);
+modalOverlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e) {
+  if (e.key == 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+})
+
